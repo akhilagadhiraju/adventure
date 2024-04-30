2a7c0515d8318968533af428c82e8a0fb3eae9d8
AkhilaGadhiraju284 <agadhira@stevens.edu>
2024-04-30 19:32:19 -0400

    i made some changes in python file

--- adventure.py
+++ adventure.py
@@ -24,42 +24,41 @@ class AdventureGame:
                 game_map = json.load(file)
             self.validate_map(game_map)
             return game_map
+        except FileNotFoundError:
+            sys.exit("Error: The map file was not found.")
+        except json.JSONDecodeError:
+            sys.exit("Error: The map file is not a valid JSON.")
         except Exception as e:
             sys.exit(f"Error loading map: {e}")
 
     def validate_map(self, game_map):
         if "start" not in game_map or "rooms" not in game_map:
             raise ValueError("Map must include 'start' and 'rooms'")
-        if not isinstance(game_map['rooms'], list) or not all("name" in room for room in game_map['rooms']):
-            raise ValueError("Each room must be a dictionary with a 'name' key")
+        if not isinstance(game_map['rooms'], list) or not all(isinstance(room, dict) and "name" in room for room in game_map['rooms']):
+            raise ValueError("Each room must be a dictionary with at least a 'name' key")
+        room_names = [room['name'] for room in game_map['rooms']]
+        if len(room_names) != len(set(room_names)):
+            raise ValueError("Room names must be unique")
+        if game_map['start'] not in room_names:
+            raise ValueError("Start room must be one of the rooms")
+        if any(set(room.get('exits', {}).values()) - set(room_names) for room in game_map['rooms']):
+            raise ValueError("All exits must point to existing rooms")
 
     def describe_room(self):
         room = self.current_room
-        print(f"\n> {room['name']}\n")
-        print(f"{room['desc']}\n")
-        
+        print(f"\n> {room['name']}")
+        print(room['desc'])
         if 'items' in room and room['items']:
-            print("Items in the room: " + ", ".join(room['items']) + "\n")
-
-        exits_formatted = ' '.join(room['exits'].keys())
-        print(f"Exits: {exits_formatted}\n")
+            print("Items in the room: " + ", ".join(room['items']))
+        print("Exits: " + ', '.join(room['exits'].keys()))
 
     def parse_command(self, command):
-        if command.startswith('go '):
-            self.handle_go(command[3:])
-        elif command.startswith('get '):
-            self.handle_get(command[4:])
-        elif command.startswith('drop '):
-            self.handle_drop(command[5:])
-        elif command == 'look':
-            self.describe_room()
-        elif command == 'inventory':
-            self.show_inventory()
-        elif command == 'quit':
-            self.running = False
-            print("Goodbye!")
-        elif command == 'help':
-            self.show_help()
+        args = command.split()
+        action = args[0]
+        option = ' '.join(args[1:])
+        method_name = f"handle_{action}"
+        if method_name in dir(self):
+            getattr(self, method_name)(option)
         else:
             print("I don't understand that command.")
 
@@ -67,6 +66,7 @@ class AdventureGame:
         if direction in self.current_room['exits']:
             new_room_name = self.current_room['exits'][direction]
             self.current_room = self.rooms[new_room_name]
+            self.describe_room()
         else:
             print("You can't go that way.")
 
@@ -75,6 +75,7 @@ class AdventureGame:
             self.inventory.append(item)
             self.current_room['items'].remove(item)
             print(f"You picked up the {item}.")
+            self.describe_room()
         else:
             print(f"There is no {item} here to pick up.")
 
@@ -85,14 +86,25 @@ class AdventureGame:
                 self.current_room['items'] = []
             self.current_room['items'].append(item)
             print(f"You dropped the {item}.")
+            self.describe_room()
         else:
             print(f"You do not have a {item} to drop.")
 
-    def show_inventory(self):
+    def handle_look(self):
+        self.describe_room()
+
+    def handle_inventory(self):
         if self.inventory:
             print("You are carrying: " + ", ".join(self.inventory))
         else:
             print("You are not carrying anything.")
+
+    def handle_help(self):
+        self.show_help()
+
+    def handle_quit(self):
+        self.running = False
+        print("Goodbye!")
 
     def show_help(self):
         print("You can run the following commands:")
@@ -100,8 +112,8 @@ class AdventureGame:
             print(f"  {desc}")
 
     def run(self):
+        self.describe_room()
         while self.running:
-            self.describe_room()
             command = input("What would you like to do? ").strip().lower()
             self.parse_command(command)
 
@@ -112,5 +124,6 @@ if __name__ == "__main__":
 
     game = AdventureGame(sys.argv[1])
     game.run()
+
 
 #comment
